2571e3d63daa9ca4122ad3c4634c1b15285c9740
Talita Gomes Ferreira <talita.ferreira@db1.com.br>
2020-08-13 23:48:42 -0300

    Changing Layout 1

---
+++ dist/index.js
@@ -0,0 +1,12 @@
+var teste = /** @class */ (function () {
+    function teste() {
+        console.log("Yup, this code is working girl.");
+    }
+    return teste;
+}());
+new teste();
+
+
+
+
+
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+class teste {
+    constructor() {
+        console.log("Yup, this code is working, girl.");
+    }
+}
+
+new teste();
+
+
+
+
+
