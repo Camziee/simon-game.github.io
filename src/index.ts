96b9cdd69ecc2e741f40532456d860f26bd245c4
Camz <talitagomesfe@gmail.com>
2020-08-17 23:37:04 -0300

    Fist Class tries

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,108 @@
-class teste {
+const winningRound = 10;
+
+
+
+class GameApp {
+    public gameRunning: boolean;
+    
+    gameState: boolean; /* check with Adriano gameState <> gameRunning */
     constructor() {
-        console.log("Yup, this code is working, girl.");
+    
     }
+
+    /* Responsibility:
+       - C
+    */
 }
 
-new teste();
+
+
+ class GameController {
+    gameRunning: boolean; /* check with Adriano gameState <> gameRunning */
+
+
+    public gameSequence: string[];
+    public playerSequence: string[];
+    
+    constructor() {
+
+    }
+
+    /* Responsibility:
+       - Random buttom
+       - Add random buttom to gameSequence
+       - Get playerButton
+       - Add playerButtom to playerSequence
+       - Compare gameSquence == playerSequence
+       - Set loseScreen = false
+       -
+       - Call Rounds
+    */
+
+ }
+
+ 
+
+
+ class GameButton {
+    public buttonState: boolean;
+
+    constructor() {
+
+    }
+
+    /* Responsibility:
+       - Add class game-button--active to div.game-button
+    */
+ }
 
 
 
+  class Rounds {
+
+    public rounds: number;
+
+    constructor() {
+
+    }
+
+    /* Responsibility:
+      - Set gameRunning = true/false
+      - Verify if rounds >= winningRound
+      - Set winScreen = true
+    */
+  }
 
 
+
+  class WinScreen {
+
+    public  winScreen: boolean;
+
+    constructor() {
+
+    }
+
+    /* Responsibility:
+      - Set winScreen = false;
+      - Set gameRunning = true/false;
+    */
+
+  }
+
+
+
+  class LoseScreen {
+
+    public loseScreen: boolean;
+
+    constructor() {
+
+    }
+
+    /* Responsibility:
+      - Set loseScreen = false;
+      - Set gameRunning = true/false;
+    */
+
+}
